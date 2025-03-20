074a7726dc0a25399040ed51d3735ef59253cadd
PeSzpak <alves.7908@aluno.pr.senac.br>
2025-03-20 17:12:26 -0300

    feat: add field validation in the from before inserting into the table

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,42 +1,48 @@
 // Cálculo do IMC do paciente
 var pacientes = document.querySelectorAll(".paciente");
- 
+
 for (var i = 0; i < pacientes.length; i++) {
     const paciente = pacientes[i];
-    var tdPeso = paciente.querySelector(".info-peso");
-    var tdAltura = paciente.querySelector(".info-altura");
- 
+
+    var peso = paciente.querySelector(".info-peso").textContent;
+
+    var altura = paciente.querySelector(".info-altura").textContent;
+
+
+    var alturaEhValida = validaAltura(altura);
+    var pesoEhValido = validaPeso(peso);
+
     var tdImc = paciente.querySelector(".info-imc");
- 
-    var alturaEhValida = true;
-    var pesoEhValido = true;
- 
-    var peso = tdPeso.textContent;
-    if (peso <= 0 || peso > 600) {
-        console.log("Peso inválido!");
-        pesoEhValido = false;
+
+    if (!pesoEhValido) {
         tdImc.textContent = "Peso inválido!"
     }
- 
-    var altura = tdAltura.textContent;
-    if (altura <= 0 || altura > 3) {
-        console.log("Altura inválida!");
-        alturaEhValida = false;
+
+    if (!alturaEhValida) {
         tdImc.textContent = "Altura inválida!";
     }
- 
+
     if (!pesoEhValido && !alturaEhValida) {
         tdImc.textContent = "Altura e/ou peso inválidos!"
     }
- 
+
     if (alturaEhValida && pesoEhValido) {
         var imc = calculaImc(peso, altura);
         tdImc.textContent = imc;
     }
 }
- 
+
 function calculaImc(peso, altura) {
     var imc = 0;
     imc = peso / Math.pow(altura, 2);
     return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+    return (peso > 0 && peso < 600) ? true : false
+}
+
+function validaAltura(altura) {
+    return (altura > 0 && altura < 3) ? true : false;
+
 }
--- js/forms.js
+++ js/forms.js
@@ -1,56 +1,99 @@
-    
-    var botaoAdicionar = document.querySelector("#adicionar-paciente");
- 
-    botaoAdicionar.addEventListener("click", function (event) {
-        event.preventDefault();
-     
-        var form = document.querySelector("#form-adiciona");
-     
-        var paciente = obtemPacienteDoFormulario(form);
 
-        if(!paciente.nome || !paciente.peso || !paciente.altura || !paciente.gordura) {
-        var validacao = document.querySelector("#msg-erro");
-    
-        validacao.textContent = "ERRO! TODOS OS CAMPOS DEVEM SER PREECHIDOS";
-        
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var paciente = obtemPacienteDoFormulario(form);
+
+    // validação
+    var erros = validaPaciente(paciente);
+    if (erros.length > 0) {
+        exibeMsgErro(erros);
         return;
-     }
-     
-        var pacienteTr = montaTr(paciente);
-     
-        var tabela = document.querySelector("#tabela-pacientes");
-     
-        tabela.appendChild(pacienteTr);
-     
-        form.reset();
-    })
-    
-    function obtemPacienteDoFormulario(form) {
-        var paciente = {
-            nome: form.nome.value,
-            peso: form.peso.value,
-            altura: form.altura.value,
-            gordura: form.gordura.value,
-            imc: calculaImc(form.peso.value, form.altura.value)
-        }
-        return paciente;
     }
-     
-    function montaTd(dado) {
-        var td = document.createElement("td");
-        td.textContent = dado;
-     
-        return td;
+
+
+    // if(!paciente.nome || !paciente.peso || !paciente.altura || !paciente.gordura) {
+    // var validacao = document.querySelector("#msg-erro");
+
+    // validacao.textContent = "ERRO! TODOS OS CAMPOS DEVEM SER PREECHIDOS";
+
+    // return;
+
+
+    var pacienteTr = montaTr(paciente);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+    form.reset();
+})
+
+
+function obtemPacienteDoFormulario(form) {
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
     }
-     
-    function montaTr(paciente) {
-        var pacienteTr = document.createElement("tr");
-     
-        pacienteTr.appendChild(montaTd(paciente.nome));
-        pacienteTr.appendChild(montaTd(paciente.peso));
-        pacienteTr.appendChild(montaTd(paciente.altura));
-        pacienteTr.appendChild(montaTd(paciente.gordura));
-        pacienteTr.appendChild(montaTd(paciente.imc));
-     
-        return pacienteTr;
+    return paciente;
+}
+
+function montaTd(dado) {
+    var td = document.createElement("td");
+    td.textContent = dado;
+
+    return td;
+}
+
+function montaTr(paciente) {
+    var pacienteTr = document.createElement("tr");
+
+    pacienteTr.appendChild(montaTd(paciente.nome));
+    pacienteTr.appendChild(montaTd(paciente.peso));
+    pacienteTr.appendChild(montaTd(paciente.altura));
+    pacienteTr.appendChild(montaTd(paciente.gordura));
+    pacienteTr.appendChild(montaTd(paciente.imc));
+
+    return pacienteTr;
+}
+
+function validaPaciente(paciente) {
+    var erros = [];
+    if (paciente.nome.length == 0) {
+        erros.push("Nome é obrigatório.");
     }
+    if (paciente.altura.length == 0) {
+        erros.push("altura é obrigatório.");
+    }
+    if (paciente.peso.length == 0) {
+        erros.push("Peso é obrigatório.")
+    }
+    if (paciente.gordura.length == 0) {
+        erros.push("Gordura é obrigatório.");
+    }
+    if (validaPeso(paciente.peso)) {
+        erros.push("Peso é invalido.");
+    }
+    if (validaAltura(paciente.altura)) {
+        erros.push("Altura é invalido.")
+    }
+    return erros;
+}
+
+function exibeMsgErro(erros) {
+    var ul = document.querySelector("#msg-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(erro => {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
